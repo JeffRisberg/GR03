a9a6d089095b78c7f8533323de0c6d01853c2e3c
Jeff Risberg <jrisberg@aol.com>
2011-12-01 04:05:07 -0800

    general maint

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,37 +1,37 @@
 //GR03
 dataSource {
-	pooled = true
-	driverClassName = "com.mysql.jdbc.Driver"
-	username = "developer"
-	password = "123456"
+  pooled = true
+  driverClassName = "com.mysql.jdbc.Driver"
+  username = "developer"
+  password = "123456"
 }
 hibernate {
-	cache.use_second_level_cache = true
-	cache.use_query_cache = true
-	cache.provider_class = 'net.sf.ehcache.hibernate.EhCacheProvider'
+  cache.use_second_level_cache = true
+  cache.use_query_cache = true
+  cache.provider_class = 'net.sf.ehcache.hibernate.EhCacheProvider'
 }
 // environment specific settings
 environments {
-	development {
-		dataSource {
-			dbCreate = "update" // one of 'create', 'create-drop','update'
-			url = "jdbc:mysql://localhost:3306/gr03?autoreconnect=true"
-			show_sql = true
-		}
-	}
-	test {
-		dataSource {
-			dbCreate = "create-drop"
-			url = "jdbc:hsqldb:file:testDb"
-			driverClassName = "org.hsqldb.jdbcDriver"
-			username = "sa"
-			password = ""
-		}
-	}
-	production {
-		dataSource {
-			dbCreate = "update"
-			url = "jdbc:mysql://localhost:3306/gr02"
-		}
-	}
+  development {
+    dataSource {
+      dbCreate = "update" // one of 'create', 'create-drop','update'
+      url = "jdbc:mysql://localhost:3306/gr03?autoReconnect=true"
+      show_sql = true
+    }
+  }
+  test {
+    dataSource {
+      dbCreate = "create-drop"
+      url = "jdbc:hsqldb:file:testDb"
+      driverClassName = "org.hsqldb.jdbcDriver"
+      username = "sa"
+      password = ""
+    }
+  }
+  production {
+    dataSource {
+      dbCreate = "update"
+      url = "jdbc:mysql://localhost:3306/gr03?autoReconnect=true"
+    }
+  }
 }
--- src/groovy/com/incra/controller/AbstractController.groovy
+++ src/groovy/com/incra/controller/AbstractController.groovy
@@ -13,119 +13,104 @@ import com.incra.User
  */
 class AbstractController {
 
-	def grailsApplication
+  def grailsApplication
 
-	/** Authenticate Service : IOC-Bean */
-	def authenticateService;
+  /** Authenticate Service : IOC-Bean */
+  def authenticateService;
 
-	/** Authenticated user domain instance */
-	public User loginUser
+  /** Authenticated user domain instance */
+  public User loginUser
 
-	/** is user logged on or not */
-	boolean logon
+  /** is user logged on or not */
+  boolean logon
 
-	/** principal */
-	def authPrincipal
+  /** principal */
+  def authPrincipal
 
-	/** is Admin */
-	boolean isAdmin
+  /** is Admin */
+  boolean isAdmin
 
-	/** is CompanyAdmin */
-	boolean isCompanyAdmin
+  /** is CompanyAdmin */
+  boolean isCompanyAdmin
 
-	/** Locale */
-	Locale locale
+  /** Locale */
+  Locale locale
 
-	/** main request permission setting */
-	def requestAllowed
+  /** main request permission setting */
+  def requestAllowed
 
-	String header;
-	String serverName
+  String header;
+  String serverName
 
-	boolean agentIsWide = true;
-	boolean agentIsWide4 = false;
-	boolean agentIsJsCapable = true;
+  def beforeInterceptor = { return beforeInterceptorExample(); }
 
-	boolean isBB;
-	boolean isTeenSite = false;
-	boolean isHttps = false;
+  def beforeInterceptorExample() {
+    header = request.getHeader("User-Agent") ?: "";
+    header = header.toLowerCase();
 
+    if (params['fromsite']) {
+      session.fromsite = params['fromsite'];
+    }
 
-	def beforeInterceptor = { return beforeInterceptorExample(); }
+    serverName = request.serverName;
+    isHttps = (request.scheme?.endsWith("s") || false);
 
-	def beforeInterceptorExample() {
-		header = request.getHeader("User-Agent") ?: "";
-		header = header.toLowerCase();
+    // TODO - Is this in use?
+    if (isBB && isHttps) {
+      String file = request.forwardURI;
+      if (request.getQueryString() != null) {
+        file += '?' + request.getQueryString();
+      }
+      URL reconstructedURL = new URL("http", //request.getScheme(),
+          request.getServerName(),
+          //                    request.getServerPort(),
+          file);
 
-		if (params['fromsite']) {
-			session.fromsite = params['fromsite'];
-		}
+      redirect(url: "${reconstructedURL.toString()}");
+      return false;
+    }
 
-		def width = 960
+    if (requestAllowed != null && !authenticateService.ifAnyGranted(requestAllowed)) {
+      redirect(uri: '/')
+      return
+    }
 
-		serverName = request.serverName;
-		isTeenSite = serverName.contains('viveit') || serverName.contains('127') || session.fromsite?.contains('viveit');
-		isHttps = (request.scheme?.endsWith("s") || false);
+    Authentication authentication = SCH?.context?.authentication
+    if (authentication) {
+      authPrincipal = authentication?.principal
+      if (authPrincipal != null && authPrincipal != 'anonymousUser') {
+        loginUser = User.get(authPrincipal?.domainClass?.id);
+      }
+      if (loginUser) {
+        session.loginUser = loginUser
+        logon = true
+        if (authenticateService) {
+          isAdmin = authenticateService.ifAnyGranted('ROLE_ADMIN')
+          isCompanyAdmin = authenticateService.ifAnyGranted('ROLE_COMPANY_ADMIN')
+        }
+      }
+    }
 
-		agentIsWide = width >= 480; //!(isIphone() || isAndroid() || isPre());
-		agentIsWide4 = width >= 960; //agentIsWide && !isBB && !isMobileOpera();
+    // i18n: if lang params
+    if (params['lang']) {
+      locale = new Locale(params['lang'])
+      RCU.getLocaleResolver(request).setLocale(request, response, locale)
+      session.lang = params['lang']
+    }
 
-		// TODO - Is this in use?
-		if (isBB && isHttps) {
-			String file = request.forwardURI;
-			if (request.getQueryString() != null) {
-				file += '?' + request.getQueryString();
-			}
-			URL reconstructedURL = new URL("http", //request.getScheme(),
-					request.getServerName(),
-					//                    request.getServerPort(),
-					file);
+    // need this for jetty
+    if (session.lang != null) {
+      locale = new Locale(session.lang)
+      RCU.getLocaleResolver(request).setLocale(request, response, locale)
+    }
 
-			redirect(url: "${reconstructedURL.toString()}");
-			return false;
-		}
+    if (locale == null) {
+      locale = RCU.getLocale(request)
+    }
 
-		if (requestAllowed != null && !authenticateService.ifAnyGranted(requestAllowed)) {
-			redirect(uri: '/')
-			return
-		}
-
-		Authentication authentication = SCH?.context?.authentication
-		if (authentication) {
-			authPrincipal = authentication?.principal
-			if (authPrincipal != null && authPrincipal != 'anonymousUser') {
-				loginUser = ViveUser.get(authPrincipal?.domainClass?.id);
-			}
-			if (loginUser) {
-				session.loginUser = loginUser
-				logon = true
-				if (authenticateService) {
-					isAdmin = authenticateService.ifAnyGranted('ROLE_ADMIN')
-					isCompanyAdmin = authenticateService.ifAnyGranted('ROLE_COMPANY_ADMIN')
-				}
-			}
-		}
-
-		// i18n: if lang params
-		if (params['lang']) {
-			locale = new Locale(params['lang'])
-			RCU.getLocaleResolver(request).setLocale(request, response, locale)
-			session.lang = params['lang']
-		}
-
-		// need this for jetty
-		if (session.lang != null) {
-			locale = new Locale(session.lang)
-			RCU.getLocaleResolver(request).setLocale(request, response, locale)
-		}
-
-		if (locale == null) {
-			locale = RCU.getLocale(request)
-		}
-
-		response.setHeader('Cache-Control', 'no-cache') // HTTP 1.1
-		response.setDateHeader('max-age', 0)
-		response.setIntHeader('Expires', -1) //prevents caching at the proxy server
-		response.addHeader('cache-Control', 'private') //IE5.x only
-	}
+    response.setHeader('Cache-Control', 'no-cache') // HTTP 1.1
+    response.setDateHeader('max-age', 0)
+    response.setIntHeader('Expires', -1) //prevents caching at the proxy server
+    response.addHeader('cache-Control', 'private') //IE5.x only
+  }
 }
